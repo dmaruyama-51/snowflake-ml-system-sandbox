49f8e38a11ab2f50bf522b1b6981848d0285e0b0
dmaruyama-51 <0501awasuram@gmail.com>
2025-01-30 10:24:28 +0900

    add sproc_dataset

--- src/adhoc/prepare_dataset.py
+++ src/adhoc/prepare_dataset.py
@@ -1,113 +1,23 @@
-import random
-import uuid
+from datetime import datetime
 
-import pandas as pd
-from snowflake.snowpark.session import Session
-from ucimlrepo import fetch_ucirepo
+from src.data.ml_dataset import create_ml_dataset
+from src.data.source import prepare_online_shoppers_data
+from src.utils.snowflake import create_session
+from src.utils.logger import setup_logging
+import logging
 
-from src.utils.snowflake import create_session, upload_dataframe_to_snowflake
-
-# MONTHカラムの値を月番号に変換する辞書
-MONTH_TO_NUM = {
-    "Jan": "01",
-    "Feb": "02",
-    "Mar": "03",
-    "Apr": "04",
-    "May": "05",
-    "June": "06",
-    "Jul": "07",
-    "Aug": "08",
-    "Sep": "09",
-    "Oct": "10",
-    "Nov": "11",
-    "Dec": "12",
-}
-
-
-def get_year(month: str) -> str:
-    """月に応じて年を設定する"""
-    return (
-        "2025"
-        if month in ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep"]
-        else "2024"
-    )
-
-
-def get_random_day(month: str) -> str:
-    """月に応じて日付をランダムに割り振る"""
-    random.seed(0)
-    days_in_month = {
-        "Apr": 30,
-        "June": 30,
-        "Sep": 30,
-        "Nov": 30,
-        "Feb": 28,  # 2024/2025年なので2月は28日
-        "Jan": 31,
-        "Mar": 31,
-        "May": 31,
-        "Jul": 31,
-        "Aug": 31,
-        "Oct": 31,
-        "Dec": 31,
-    }
-    return str(random.randint(1, days_in_month[month])).zfill(2)
-
-
-def prepare_online_shoppers_data(
-    session: Session,
-    database_name: str,
-    schema_name: str,
-    table_name: str,
-    mode: str = "overwrite",
-) -> None:
-    """
-    Online Shoppers Intention データセットを取得し、Snowflakeにロードする
-
-    Args:
-        session (AsyncSession): Snowflakeセッション
-        database_name (str): ロード先のデータベース名
-        schema_name (str): ロード先のスキーマ名
-        table_name (str): ロード先のテーブル名
-        mode (str, optional): データ書き込みモード. Defaults to 'overwrite'.
-
-    Raises:
-        Exception: データの取得中にエラーが発生した場合
-    """
-    try:
-        # データセットの取得
-        dataset = fetch_ucirepo(id=468)
-        df: pd.DataFrame = dataset.data.features
-        df_target: pd.DataFrame = dataset.data.targets
-        df["revenue"] = df_target["Revenue"]
-
-        df["SESSION_DATE"] = pd.to_datetime(
-            df["Month"].apply(get_year)
-            + df["Month"].map(MONTH_TO_NUM)
-            + df["Month"].apply(get_random_day)
-        )
-        # ユーザーIDをランダムに生成
-        df["UID"] = [str(uuid.uuid4()) for _ in range(len(df))]
-
-        # Snowflakeへのアップロード
-        upload_dataframe_to_snowflake(
-            session=session,
-            df=df,
-            database_name=database_name,
-            schema_name=schema_name,
-            table_name=table_name,
-            mode=mode,
-        )
-
-    except Exception as e:
-        print(f"エラーが発生しました: {str(e)}")
-        raise
-
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     try:
+        setup_logging()
+
+        
+        logger.info("Creating Snowflake session")
         session = create_session()
         if session is None:
-            raise ValueError("Snowflakeセッションの作成に失敗しました")
+            raise ValueError("Failed to create Snowflake session")
+        logger.info("Snowflake session created successfully")
 
         prepare_online_shoppers_data(
             session=session,
@@ -115,6 +25,17 @@ if __name__ == "__main__":
             schema_name="ml",
             table_name="online_shoppers_intention",
         )
+
+        today = datetime.now().strftime("%Y-%m-%d")
+        create_ml_dataset(
+            session=session,
+            target_date=today,
+            database_name="practice",
+            schema_name="ml",
+            table_name="dataset",
+            source_table_name="online_shoppers_intention",
+        )
     finally:
         if session:
+            logger.info("Closing Snowflake session")
             session.close()
---
+++ src/pipelines/sproc_dataset.py
@@ -0,0 +1,82 @@
+import logging
+import os
+import sys
+
+from snowflake.snowpark import Session
+
+from src.data.ml_dataset import update_ml_dataset
+from src.utils.logger import setup_logging
+from src.utils.snowflake import create_session
+
+logger = logging.getLogger(__name__)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+IMPORTS_DIR = os.path.join(BASE_DIR, "src")
+
+
+def sproc_dataset(session: Session, target_date: str) -> int:
+    """
+    推論Sprocの処理内容
+
+    Args:
+        session (Session): Snowflakeセッション
+        prediction_date (str): 推論日付（YYYY-MM-DD）
+
+    Returns:
+        int: 成功時は1、失敗時は例外を発生
+
+    Raises:
+        Exception: 処理中にエラーが発生した場合
+    """
+    try:
+        setup_logging()
+        _ = update_ml_dataset(
+            session, 
+            database_name=session.get_current_database(),
+            schema_name=session.get_current_schema(),
+            target_date=target_date,
+            table_name="dataset",
+            source_table_name="online_shoppers_intention",
+        )
+        return 1
+
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        raise e
+
+
+if __name__ == "__main__":
+    try:
+        session = create_session()
+        if session is None:  # セッションがNoneの場合のチェックを追加
+            raise RuntimeError("Failed to create Snowflake session")
+
+        sproc_config = {
+            "name": "DATASET",
+            "is_permanent": True,
+            "stage_location": "@practice.ml.sproc",
+            "packages": [
+                "snowflake-snowpark-python"
+            ],
+            "imports": [
+                (os.path.join(IMPORTS_DIR, "data"), "src.data"),
+                (os.path.join(IMPORTS_DIR, "utils/logger.py"), "src.utils.logger"),
+                (
+                    os.path.join(IMPORTS_DIR, "utils/snowflake.py"),
+                    "src.utils.snowflake",
+                ),
+            ],
+            "replace": True,
+            "execute_as": "caller",
+        }
+        session.sproc.register(func=sproc_dataset, **sproc_config)  # type: ignore
+        session.sql(
+            "ALTER PROCEDURE DATASET(VARCHAR) SET LOG_LEVEL = 'INFO'"
+        ).collect()
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        sys.exit(1)
+    finally:
+        if session:
+            session.close()
